62f140f83b0be71822466ed6eda53ef8d8a08025
gdigugli <gdigugli@gmail.com>
2012-04-17 19:24:42 +0200

    added devoxx_storyboard.txt

--- conf4j-service/src/test/java/org/conf4j/service/SampleServiceWithConfTest.java
+++ conf4j-service/src/test/java/org/conf4j/service/SampleServiceWithConfTest.java
@@ -1,6 +1,7 @@
 package org.conf4j.service;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 
@@ -13,12 +14,12 @@ public class SampleServiceWithConfTest {
     private final SampleServiceWithConf svc = new SampleServiceWithConf();
 
     @Test
-    public void testDevoxxHome_localhost() {
-        assertFalse("http://localhost/display/FR12/Accueil".equals(svc.getDevoxxHome()));
+    public void testDevoxxUnitTest_localhost() {
+        assertFalse("http://localhost/display/FR12/UnitTest".equals(svc.getDevoxxUnitTest()));
     }
 
     @Test
-    public void testDevoxxHome_test_devoxx_fr() {
-        assertEquals("http://test.devoxx.fr/display/FR12/Accueil", svc.getDevoxxHome());
+    public void testDevoxxUnitTest_test_devoxx_fr() {
+        assertEquals("http://test.devoxx.fr/display/FR12/UnitTest", svc.getDevoxxUnitTest());
     }
 }
